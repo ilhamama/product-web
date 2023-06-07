11fb058a3763fb02fc751919fff78b35debd6685
Ilham Amajidi <ama.ilham95@gmail.com>
2023-06-07 16:22:34 +0700

    Initial product web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+  products: Product[] = [];
+  currentPage = 1;
+  totalPages = 0;
+  pages: number[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.getProducts();
+    this.totalPages = 5;
+    this.pages = [1, 2, 3, 4, 5]
+  }
+
+  getProducts(): void {
+    this.products = [
+      {code: 'P0001', name: 'Realme 5 Pro', category: 'Mobile & Gadgets', brand: 'Realme', type: 'Mobile Phones', description: 'REALME 5 PRO 6+128GB'},
+      {code: 'P0002', name: 'Nokia 2.3', category: 'Mobile & Gadgets', brand: 'Nokia', type: 'Mobile Phones', description: 'Cyan Green'},
+      {code: 'P0003', name: 'Red Dinner Gown', category: 'Fashin', brand: '', type: '', description: '1234'}
+    ]
+  }
+
+  changePage(page: number): void {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.getProducts();
+    }
+  }
+}
+
+interface Product {
+  code: string;
+  name: string;
+  category: string;
+  brand: string;
+  type: string;
+  description: string;
+}
